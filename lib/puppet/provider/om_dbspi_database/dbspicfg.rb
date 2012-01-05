f7511b1620148af36a3fbeb7fc0928a12273d595
Stefan Schulte <stefan.schulte@taunusstein.net>
2012-01-05 23:33:19 +0100

    Add dbspicfg provider for om_dbspi_database type
    The dbspicfg provider uses the dbspicfg binary that is deliviered as an
    instrumentation when deploying the dbspi policies to a database node.
    
    Normally you would execute dbspicfg.sh to open up an editor and modify
    the current configuration.  The dbspicfg binary on the other hand
    understands the arguments -e (export) and -i (import) so we can
    
        1) extract the configuration and parse it
        2) modifiy it in memory
        3) generate a temporary file for the new configuration (might include
           plain text passwords!)
        4) load the new configuration
        5) remove the temp file
    
    That is how the provider currently works

---
+++ lib/puppet/provider/om_dbspi_database/dbspicfg.rb
@@ -0,0 +1,144 @@
+Puppet::Type.type(:om_dbspi_database).provide(:dbspicfg) do
+
+  desc "Uses the dbspicfg command to export and import the
+      current dbspi configuration"
+
+  commands :dbspicfg => '/var/opt/OV/bin/instrumentation/dbspicfg'
+
+  mk_resource_methods
+
+  def self.initvars
+    @globalconfig = {}
+    @records = {}
+  end
+
+  def self.parse_next_token(scanner)
+    scanner.skip(/(\s+|\n+|#.*?\n)+/)
+    if scanner.scan(/"(.*?)"/) or scanner.scan(/(\S+)/)
+      scanner[1]
+    end
+  end
+
+  def self.instances
+    self.initvars
+    current_record = nil
+    last_setting = {}
+    scanner = StringScanner.new(dbspicfg('-e'))
+    until scanner.eos?
+      case token = parse_next_token(scanner)
+      when 'SYNTAX_VERSION'
+        @globalconfig['SYNTAXVERSION'] = parse_next_token(scanner)
+        current_record = nil
+      when 'ORACLE'
+        last_setting['DBTYPE'] = :oracle
+        @records[current_record[:name]] = current_record if current_record
+        current_record = nil
+      when 'HOME'
+        last_setting['HOME'] = parse_next_token(scanner)
+        @records[current_record[:name]] = current_record if current_record
+        current_record = nil
+      when 'DATABASE'
+        @records[current_record[:name]] = current_record if current_record
+        last_setting['DATABASE'] = parse_next_token(scanner)
+        current_record = {
+          :ensure => :present,
+          :name   => last_setting['DATABASE'],
+          :home   => last_setting['HOME'],
+          :type   => last_setting['DBTYPE'],
+          :filter => {}
+        }
+        if @records.include? current_record[:name]
+          warning "Found duplicate database entry: #{current_record[:name]}"
+        end
+      when 'CONNECT'
+        connect_string = parse_next_token(scanner)
+        if current_record.nil?
+          @globalconfig['LISTENER_CONNECT'] = connect_string
+        else
+          current_record[:connect] = connect_string
+        end
+      when 'LOGFILE'
+        current_record[:logfile] = parse_next_token(scanner) unless current_record.nil?
+      when 'FILTER'
+        metric = parse_next_token(scanner)
+        filter = parse_next_token(scanner)
+        unless current_record.nil?
+          current_record[:filter][metric.intern] = filter
+        end
+      when 'LISTENER'
+        @records[current_record[:name]] = current_record if current_record
+        current_record = nil
+        @globalconfig['LISTENER'] = parse_next_token(scanner)
+      else
+        warning "Found unrecognized token: #{token}"
+      end
+      scanner.skip(/(\s+|\n+|#.*?\n)+/)
+    end
+    @records[current_record[:name]] = current_record if current_record
+
+    # convert our hash array into true provider instances
+    @records.values.map { |r| new(r) }
+  end
+
+  def self.prefetch(resources)
+    instances.each do |prov|
+      if resource = resources[prov.name]
+        resource.provider = prov
+      end
+    end
+  end
+
+  def self.flush(record)
+    tempfile = Tempfile.new('dbspi')
+    if record[:ensure] == :absent
+      @records.delete(record[:name])
+    else
+      @records[record[:name]] = record
+    end
+    begin
+      if syntax_version = @globalconfig['SYNTAX_VERSION']
+        stdin_file.write "SYNTAX_VERSION #{syntax_version}\n"
+        stdin_file.write ""
+      end
+      current_dbtype = nil
+      current_home = nil
+      @records.values.sort_by { |r| [r[:type], r[:home], r[:name]] }.each do |record|
+        if current_dbtype != record[:type]
+          tempfile.write "#{record[:type]}\n"
+        end
+        if current_home != record[:home]
+          tempfile.write "  HOME \"#{record[:home]}\"\n"
+        end
+        if record[:connect]
+          tempfile.write "    DATABASE \"#{record[:name]}\" CONNECT \"#{record[:connect]}\"\n"
+        else
+          tempfile.write "    DATABASE \"#{record[:name]}\"\n"
+        end
+        if record[:logfile]
+          tempfile.write "      LOGFILE \"#{record[:logfile]}\"\n"
+        end
+        record[:filter].each do |k,v|
+          tempfile.write "      FILTER #{k} \"#{v}\"\n"
+        end
+      end
+      if listener = @globalconfig['LISTENER']
+        if connect_string = @globalconfig['LISTENER_CONNECT']
+          tempfile.write "  LISTENER \"#{listener}\" CONNECT \"#{connect_string}\"\n"
+        else
+          tempfile.write "  LISTENER \"#{listener}\"\n"
+        end
+      end
+      tempfile.close
+      execute([command(:dbspicfg), '-i'], :stdinfile => tempfile.path)
+    ensure
+      tempfile.close # doesnt matter if file is already closed
+      tempfile.unlink
+    end
+  end
+
+  def flush
+    @property_hash[:name] ||= resource[:name]
+    self.class.flush(@property_hash)
+  end
+
+end
---
+++ spec/unit/provider/om_dbspi_database/dbspicfg_spec.rb
@@ -0,0 +1,153 @@
+#!/usr/bin/env ruby
+
+require 'spec_helper'
+
+describe Puppet::Type.type(:om_dbspi_database).provider(:dbspicfg) do
+
+  before :each do
+    described_class.stubs(:suitable?).returns true
+    Puppet::Type.type(:om_dbspi_database).stubs(:defaultprovider).returns described_class
+  end
+
+  after :each do
+    described_class.initvars
+  end
+
+  it "should support prefetching" do
+    described_class.should respond_to :prefetch
+  end
+
+  it "should support instances" do
+    described_class.should respond_to :instances
+  end
+
+  [:connect,:logfile,:filter,:home].each do |property|
+    it "should have getter and setter for property #{property}" do
+      described_class.new.should respond_to property
+      described_class.new.should respond_to "#{property}=".intern
+    end
+  end
+
+  describe "when running instances" do
+
+    it "should parse a single database entry" do
+      described_class.stubs(:dbspicfg).with('-e').returns File.read(my_fixture('simple'))
+      described_class.expects(:warning).never
+      instances = described_class.instances.sort_by {|p| p.get(:name)}
+      instances[0].get(:name).should == 'OMLE'
+      instances[0].get(:connect).should == 'itouser/secret@host:1521/OMLE'
+      instances[0].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omle/OMLE/trace/alert_OMLE.log'
+      instances[0].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[0].get(:type).should == :oracle
+      instances[0].get(:filter).should == {}
+    end
+
+    it "should parse multiple databases" do
+      described_class.stubs(:dbspicfg).with('-e').returns File.read(my_fixture('filter'))
+      described_class.expects(:warning).never
+      instances = described_class.instances.sort_by {|p| p.get(:name)}
+
+      instances[0].get(:name).should == 'OMLE'
+      instances[0].get(:connect).should == 'itouser/secret@host:1521/OMLE'
+      instances[0].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omle/OMLE/trace/alert_OMLE.log'
+      instances[0].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[0].get(:type).should == :oracle
+      instances[0].get(:filter).should == {
+        :'16' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')",
+        :'206' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')"
+      }
+
+      instances[1].get(:name).should == 'OMLP'
+      instances[1].get(:connect).should == 'itouser/secret@host:1521/OMLP'
+      instances[1].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omlp/OMLP/trace/alert_OMLP.log'
+      instances[1].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[1].get(:type).should == :oracle
+      instances[1].get(:filter).should == {
+        :'16' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')",
+        :'206' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')"
+      }
+    end
+
+    it "should parse input with weired style" do
+      described_class.stubs(:dbspicfg).with('-e').returns File.read(my_fixture('mixlines'))
+      described_class.expects(:warning).never
+      instances = described_class.instances.sort_by {|p| p.get(:name)}
+
+      instances[0].get(:name).should == 'OMLE'
+      instances[0].get(:connect).should == 'itouser/secret@host:1521/OMLE'
+      instances[0].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omle/OMLE/trace/alert_OMLE.log'
+      instances[0].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[0].get(:type).should == :oracle
+      instances[0].get(:filter).should == {
+        :'16' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')",
+        :'206' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')"
+      }
+
+      instances[1].get(:name).should == 'OMLP'
+      instances[1].get(:connect).should == 'itouser/secret@host:1521/OMLP'
+      instances[1].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omlp/OMLP/trace/alert_OMLP.log'
+      instances[1].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[1].get(:type).should == :oracle
+      instances[1].get(:filter).should == {
+        :'16' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')",
+        :'206' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')"
+      }
+    end
+
+    it "should parse input with multiple homes and databases" do
+      described_class.stubs(:dbspicfg).with('-e').returns File.read(my_fixture('multihomes'))
+      described_class.expects(:warning).never
+      instances = described_class.instances.sort_by {|p| p.get(:name)}
+
+      instances.size.should == 3
+
+
+      instances[0].get(:name).should == 'FOO'
+      instances[0].get(:connect).should == 'itouser/secret@host:1521/FOO'
+      instances[0].get(:logfile).should == '/u01/app/oracle/diag/rdbms/foo/FOO/trace/alert_FOO.log'
+      instances[0].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_2'
+      instances[0].get(:type).should == :oracle
+      instances[0].get(:filter).should == {
+        :'200' => "y not in z",
+        :'201' => "foo in bar"
+      }
+
+
+      instances[1].get(:name).should == 'OMLE'
+      instances[1].get(:connect).should == 'itouser/secret@host:1521/OMLE'
+      instances[1].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omle/OMLE/trace/alert_OMLE.log'
+      instances[1].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[1].get(:type).should == :oracle
+      instances[1].get(:filter).should == {
+        :'16' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')",
+        :'206' => "tablespace_name not in (select tablespace_name from dba_tablespaces where contents = 'UNDO')"
+      }
+
+
+      instances[2].get(:name).should == 'OMLP'
+      instances[2].get(:connect).should == 'itouser/secret@host:1521/OMLP'
+      instances[2].get(:logfile).should == '/u01/app/oracle/diag/rdbms/omlp/OMLP/trace/alert_OMLP.log'
+      instances[2].get(:home).should == '/u01/app/oracle/product/11.2.0/dbhome_1'
+      instances[2].get(:type).should == :oracle
+      instances[2].get(:filter).should == {
+        :'118' => "x not in y"
+      }
+    end
+
+    it "should warn about duplicate database entries" do
+      described_class.stubs(:dbspicfg).with('-e').returns File.read(my_fixture('duplicate'))
+      described_class.expects(:warning).with('Found duplicate database entry: OMLE')
+      described_class.instances
+    end
+
+    it "should warn about invalid tokens" do
+      described_class.stubs(:dbspicfg).with('-e').returns File.read(my_fixture('invalidtoken'))
+      described_class.expects(:warning).with('Found unrecognized token: ORACLCE')
+      described_class.expects(:warning).with('Found unrecognized token: LOGGFILE')
+      described_class.expects(:warning).with('Found unrecognized token: /u01/app/oracle/diag/rdbms/omle/OMLE/trace/alert_OMLE.log')
+      described_class.instances
+    end
+
+  end
+
+end
