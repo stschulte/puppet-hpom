a968f65ec19870a3315963d54418943bd71c0160
Stefan Schulte <stefan.schulte@taunusstein.net>
2013-02-13 20:19:56 +0100

    Retrieve opcagtversion with ovconfget
    The ovconfget command is a lot faster than the opcagt command so use
    this one instead.

--- lib/facter/opcagtversion.rb
+++ lib/facter/opcagtversion.rb
@@ -1,5 +1,5 @@
 Facter.add(:opcagtversion) do
   setcode do
-    Facter::Util::Resolution.exec('/opt/OV/bin/opcagt -version')
+    Facter::Util::Resolution.exec('/opt/OV/bin/ovconfget eaagt OPC_INSTALLED_VERSION')
   end
 end
--- spec/unit/opcagtversion_spec.rb
+++ spec/unit/opcagtversion_spec.rb
@@ -11,7 +11,7 @@ describe Facter::Util::Fact, 'opcagtversion' do
 
   describe "when the HP Operations Manager agent is installed" do
     it "should return the version" do
-      Facter::Util::Resolution.stubs(:exec).with('/opt/OV/bin/opcagt -version').returns('11.01.056')
+      Facter::Util::Resolution.expects(:exec).with('/opt/OV/bin/ovconfget eaagt OPC_INSTALLED_VERSION').returns('11.01.056')
       Facter.fact(:opcagtversion).value.should == '11.01.056'
     end
   end
@@ -19,7 +19,7 @@ describe Facter::Util::Fact, 'opcagtversion' do
   describe "when the HP Operations Manager is not installed" do
     it "should not raise an error" do
       FileTest.stubs(:exists?).with('/opt/OV/bin/opcagt').returns false
-      proc { Facter.fact(:opcagtversion).value }.should_not raise_error
+      expect { Facter.fact(:opcagtversion).value }.to_not raise_error
     end
     it "should return nil" do
       Facter.clear
